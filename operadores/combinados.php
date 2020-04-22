f3db8ec48f3bbe884056109572330baf1182ecd8
Elton Fonseca <elton869@gmail.com>
2020-04-22 17:28:59 -0300

    04 - Operadores Combinados

---
+++ operadores/combinados.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero = 100;
+$numero = $numero + 10;
+$numero += 10;
+
+echo "$numero <br>";
+
+$string = "TreinaWeb ";
+$string = $string . "Cursos";
+$string .= " A melhor escola online do Brasil";
+
+echo $string;
+
