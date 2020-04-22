aa698f5e12e0ac7ba7825de9f06facbac9512d91
Elton Fonseca <elton869@gmail.com>
2020-04-22 17:55:42 -0300

    05 - Operador ternário

---
+++ operadores/ternario.php
@@ -0,0 +1,15 @@
+<?php
+
+$nota = 10;
+
+// if ($nota > 5) {
+//     $resultado = "Aprovado";
+// } else {
+//     $resultado = "Reprovado";
+// }
+
+// condição ? verdadeiro : falso;
+
+$resultado = $nota > 5 ? "Aprovado" : "Reprovado";
+
+echo $resultado;
