06b1f397b31e00e29bf441637d77739a1a0d7c52
Elton Fonseca <elton869@gmail.com>
2020-04-28 16:20:57 -0300

    11 - Tipo Inteiro

---
+++ tipos/inteiro.php
@@ -0,0 +1,19 @@
+<?php
+
+// decbin($decimal) <> bindec($binario)
+// decoct($decimal) <> octdec($octal)
+// dechex($decimal) <> hexdec($hexadecimal) 
+
+$dec = 980;
+$bin = 0b1111010100;
+$oct = 01724;
+$hex = 0x3d4;
+
+var_dump($dec, $bin, $oct, $hex);
+
+$int = PHP_INT_MAX;
+
+var_dump($int + 1);
+
+
+
