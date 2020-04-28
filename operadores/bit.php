f8e7cdd9afd5138c1a58ab9e05ccd80aee061c8c
Elton Fonseca <elton869@gmail.com>
2020-04-28 11:03:55 -0300

    09 - Operadores bit a bit

---
+++ operadores/bit.php
@@ -0,0 +1,39 @@
+<?php
+
+var_dump(decbin(12));
+
+var_dump(decbin(9));
+
+var_dump(bindec(1000));
+
+var_dump(12 & 9);
+
+var_dump(bindec(1101));
+
+var_dump(12 | 9);
+
+var_dump(bindec(101));
+
+var_dump(12 ^ 9);
+
+var_dump(~12);
+
+var_dump(bindec(100101));
+
+var_dump(75 >> 1);
+
+var_dump(floor(75 / 2));
+
+var_dump(bindec(10010));
+
+var_dump(75 >> 2);
+
+var_dump(bindec(10010110));
+
+var_dump(75 << 1);
+
+var_dump(75 * 2);
+
+var_dump(bindec(100101100));
+
+var_dump(75 << 2);
