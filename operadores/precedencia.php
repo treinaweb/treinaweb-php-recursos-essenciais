dc4b6970ca76f71f2794c8d5573e4c6aab0fe412
Elton Fonseca <elton869@gmail.com>
2020-04-28 12:08:38 -0300

    10 - Precedência de operadores

---
+++ operadores/precedencia.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 12 + 3;
+
+var_dump($a);
+
+var_dump(2 + 3 * 4);
+var_dump(2 + 12);
+var_dump(14);
+
+var_dump((2 + 3) * 4);
+var_dump(5 * 4);
+var_dump(20);
+
+var_dump(3 + 4 - 5 + 5);
+var_dump(7 - 5 + 5);
+var_dump(2 + 5);
+var_dump(7);
+
+var_dump(3 + (4 - (5 + 5)));
+var_dump(3 + (4 - 10));
+var_dump(3 + -6);
+var_dump(-3);
