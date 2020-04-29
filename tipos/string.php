2f59fc235b6d3aac78cc6291e1b2e3291bda4bfc
Elton Fonseca <elton869@gmail.com>
2020-04-29 16:21:13 -0300

    17 - Escapando caracteres no PHP

--- tipos/string.php
+++ tipos/string.php
@@ -7,3 +7,15 @@ var_dump($nome[10]);
 $nome[10] = "c";
 
 var_dump($nome);
+
+//echo "<h1 style="color: red;">Treinaweb Cursos</h1>";
+
+// echo "<h1 style='color: red;'>Treinaweb Cursos</h1>";
+// echo '<h1 style="color: red;">Treinaweb Cursos</h1>';
+
+// echo "<h1 style=\"color: red;\">Treinaweb Cursos</h1>";
+// echo '<h1 style=\'color: red;\'>Treinaweb Cursos</h1>';
+
+echo "Eu tenho R\$mil reais <br>";
+
+echo "\\192.168.10.10\minha-pasta\\";
