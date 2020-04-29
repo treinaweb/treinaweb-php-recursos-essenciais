9942a5a5c157698c1a9f7f2b13ff42bdd574336e
Elton Fonseca <elton869@gmail.com>
2020-04-29 19:34:30 -0300

    18 - Heredoc e Nowdoc

--- tipos/string.php
+++ tipos/string.php
@@ -1,12 +1,12 @@
 <?php
 
-$nome = "Treinaweb Cursos";
+// $nome = "Treinaweb Cursos";
 
-var_dump($nome[10]);
+// var_dump($nome[10]);
 
-$nome[10] = "c";
+// $nome[10] = "c";
 
-var_dump($nome);
+// var_dump($nome);
 
 //echo "<h1 style="color: red;">Treinaweb Cursos</h1>";
 
@@ -16,6 +16,21 @@ var_dump($nome);
 // echo "<h1 style=\"color: red;\">Treinaweb Cursos</h1>";
 // echo '<h1 style=\'color: red;\'>Treinaweb Cursos</h1>';
 
-echo "Eu tenho R\$mil reais <br>";
+// echo "Eu tenho R\$mil reais <br>";
 
-echo "\\192.168.10.10\minha-pasta\\";
+// echo "\\192.168.10.10\minha-pasta\\";
+
+$escola = "Treinaweb";
+$curso = "PHP";
+
+// $conteudo = <<<HTML
+//     <h1 style="color: red">Ola $escola</h1>
+//     <p style='color: blue'>Bem vindo ao curso de $curso<p>
+// HTML;
+
+$conteudo = <<<'HTML'
+    <h1 style="color: red">Ola $escola \</h1> 
+    <p style='color: blue'>Bem vindo ao curso de $curso<p>
+HTML;
+
+echo $conteudo;
