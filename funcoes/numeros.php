dbc99c64dd43bee2936f52ccc3eee6ec5b7619d3
Elton Fonseca <elton869@gmail.com>
2020-04-29 15:18:02 -0300

    15 - Funções para trabalhar com números

---
+++ funcoes/numeros.php
@@ -0,0 +1,22 @@
+<?php
+
+var_dump(abs(-6));
+var_dump(abs(6));
+
+var_dump(round(12.4));
+var_dump(round(12.5));
+var_dump(round(12.6));
+
+var_dump(round(12.6189, 2));
+var_dump(round(12.6129, 2));
+
+var_dump(ceil(14.1));
+var_dump(floor(14.9));
+
+var_dump(mt_rand(1, 60));
+
+var_dump(pow(3, 2));
+var_dump(3**2);
+
+var_dump(sqrt(9));
+var_dump(sqrt(10));
