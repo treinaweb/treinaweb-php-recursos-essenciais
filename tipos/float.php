cbfbde5b5649f769b1bdc3eba3186370c24d40d8
Elton Fonseca <elton869@gmail.com>
2020-04-29 10:59:29 -0300

    13 - Melhorando a leitura de números grandes

--- tipos/float.php
+++ tipos/float.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set("precision", 17);
+//ini_set("precision", 17);
 
 $n1 = 10.23;
 $n2 = .23;
@@ -12,3 +12,7 @@ var_dump($n1, $n2, $n3, $n4);
 var_dump(.1 + .2);
 
 var_dump((int) ((.1 +.7)*10));
+
+var_dump(1_000_000);
+
+var_dump(75_000.98);
