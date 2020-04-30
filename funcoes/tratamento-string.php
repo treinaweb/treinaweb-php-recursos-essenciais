881428aef084425e4069ad6dd5b150c34ff1fbd7
Elton Fonseca <elton869@gmail.com>
2020-04-30 15:00:51 -0300

    22 - Funções de tratamento de string

---
+++ funcoes/tratamento-string.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(strtoupper('Treinaweb Cursos'));
+
+var_dump(strtolower('Treinaweb Cursos'));
+
+var_dump(ucfirst('treinaweb cursos'));
+
+var_dump(ucwords('treinaweb cursos online'));
+
+var_dump(ltrim('   treinaweb   '));
+var_dump(rtrim('   treinaweb   '));
+var_dump(trim('   treinaweb   '));
