84286037a4ae1488001f066c2493fdddf3be27cb
Elton Fonseca <elton869@gmail.com>
2020-04-30 12:05:29 -0300

    20 - Funções para formatação de string via padrão

--- funcoes/formatacao-string.php
+++ funcoes/formatacao-string.php
@@ -11,5 +11,21 @@ echo "Curso de $curso versao $versao";
 echo "<br>";
 
 $resultado = 'Curso de ' . $curso . ' versao ' . $versao;
-
 echo $resultado;
+echo "<br>";
+
+$padrao = "Curso de %s versao %0.1f";
+$resultado = sprintf($padrao, $curso, $versao);
+echo $resultado;
+echo "<br>";
+
+printf($padrao, $curso, $versao);
+echo "<br>";
+
+$entrada = ['PHP', 7.4];
+
+$resultado = vsprintf($padrao, $entrada);
+echo $resultado;
+echo "<br>";
+
+vprintf("Curso de %s versao %0.1f", $entrada);
