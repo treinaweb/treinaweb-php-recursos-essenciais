49fe5b5460d4260039cbb03fb0bf76dfac9f52a1
Elton Fonseca <elton869@gmail.com>
2020-04-30 11:36:27 -0300

    19 - Mostrando Strings no PHP

---
+++ funcoes/formatacao-string.php
@@ -0,0 +1,15 @@
+<?php
+
+$curso = "PHP";
+$versao = 7.4;
+
+echo 'Curso de ', $curso, ' versao ', $versao;
+echo "<br>";
+echo 'Curso de ' . $curso . ' versao ' . $versao;
+echo "<br>";
+echo "Curso de $curso versao $versao";
+echo "<br>";
+
+$resultado = 'Curso de ' . $curso . ' versao ' . $versao;
+
+echo $resultado;
