1d694463234ad23f07206ef09a429062302d4965
Elton Fonseca <elton869@gmail.com>
2020-04-30 14:19:15 -0300

    21 - Comparação avançadas de strings

---
+++ funcoes/comparacao-string.php
@@ -0,0 +1,11 @@
+<?php
+
+var_dump('Treinaweb' === 0);
+
+var_dump('11t' === 11);
+
+var_dump(strcmp('aa', 'aaa'), 'aa' <=> 'aaa');
+
+if (strcasecmp('Aa', 'aa') === 0) {
+    echo "as strings são seguramente iguais";
+}
