e7b6dc7137a384db4c3932ced8732c31413e8fca
Elton Fonseca <elton869@gmail.com>
2020-05-06 08:55:06 -0300

    29 - Trabalhando com timestamp

--- data/obter.php
+++ data/obter.php
@@ -2,8 +2,21 @@
 
 //date_default_timezone_set('America/Sao_Paulo');
 
-$data = date("d/m/Y H:i");
+//$amanha = time() + (60*60*24);
 
-var_dump($data);
+$amanha = strtotime("2020-05-06 15:59");
+$data = date("d/m/Y H:i", $amanha);
 
-var_dump(date('e'), date('P'));
+//var_dump($data);
+
+//var_dump(date('e'), date('P'));
+
+//var_dump(time());
+
+function formata_data_para_br($data) {
+    $timestamp = strtotime($data);
+
+    return date("d/m/Y", $timestamp);
+}
+
+echo formata_data_para_br('2020-12-31');
